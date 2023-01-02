9a715d08934fa5e52adb953080706752b29b9c63
Guillaume Danguy <42386930+stykoss@users.noreply.github.com>
2023-01-02 17:01:37 +0100

    add first

---
+++ script.js
@@ -0,0 +1,9 @@
+//Pass the id of the <input> element to be copied as a parameter to the copy()
+
+let copy = (textId) => {
+    //Selects the text in the <input> elemet
+    document.getElementById(textId).select();
+    //Copies the selected text to clipboard
+    document.execCommand("copy");
+  };
+  
